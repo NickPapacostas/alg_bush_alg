c5b226cf6361865f1a0c2272894817ebddd0736c
NickPapacostas <np1249a@gmail.com>
2017-08-21 21:30:35 +0300

    adding tests and improved alg

--- lib/bush_alg.ex
+++ lib/bush_alg.ex
@@ -14,16 +14,28 @@ defmodule BushAlg do
     Enum.max([height(left), height(right)]) + 1
   end
 
-
-  @spec concat(bush, bush) :: fork
-  def concat(left, right) do 
-    {left, right}
-  end
+  def height([one]), do: one
 
   @spec build(list(bush)) :: bush
-  def build([single]), do: single
-  def build([first, second | bushes]) do
-    build([ concat(first, second) | bushes ])
+  def build([{left, right}]), do: {left, right}
+  def build([first, second, third | []]) do
+    [sfirst, ssecond, sthird] = Enum.sort_by([first, second, third], fn(bush) -> BushAlg.height(bush) end)  
+    {{sfirst, ssecond}, sthird}
+  end
+  def build([first, second | bushes]), do: build( bushes ++ [{first, second}] )
+
+  
+  def build_merge([first, second]), do: {first, second}
+  def build_merge([_first, _second, _third] = list) do 
+    [sfirst, ssecond, sthird] = Enum.sort_by(list, fn(bush) -> BushAlg.height(bush) end)  
+    {{sfirst, ssecond}, sthird}
   end
 
+  def build_merge(bushes) do 
+    case Enum.split(bushes, div(length(bushes), 2)) do
+      {[], first} -> first
+      {first_half, second_half} ->
+        {build_merge(first_half), build_merge(second_half)}
+    end
+  end
 end
--- test/bush_alg_test.exs
+++ test/bush_alg_test.exs
@@ -2,7 +2,6 @@ defmodule BushAlgTest do
   use ExUnit.Case
   doctest BushAlg
 
-
   test "bush height" do
     assert BushAlg.height(20) == 20
     assert BushAlg.height({5, 6}) == 7
@@ -22,7 +21,51 @@ defmodule BushAlgTest do
   describe "build" do 
     test "it takes a list of bushes and concatonates them" do 
       bushes = [{1,2}, 5, {5, 6}]
-      assert BushAlg.build(bushes) == { {{1,2}, 5}, {5, 6} }
+      assert BushAlg.build(bushes) == {{{1, 2}, 5}, {5, 6}}
     end
+
+    test "more bushes" do 
+      bushes = [10, {5, {6,8}}, {1,2}, 5, {5, 6}]
+      assert BushAlg.build(bushes) ==  {{5, {5, 6}}, {{{1, 2}, {5, {6, 8}}}, {5, {6, 8}}}}
+    end
+
+    test "builds optimal height bushes" do 
+      example = [4,2,3,5,1,4,6]      
+      assert BushAlg.height( BushAlg.build(example)) == 7
+    end
+  end
+
+  describe "build merge" do 
+    test "simple example" do 
+      bushes = [10, 5]
+      assert BushAlg.build_merge(bushes) == {10, 5}
+    end
+
+    test "another simple example" do 
+      bushes = [10, {6, 3}]
+      assert BushAlg.build_merge(bushes) == {10, {6, 3}}
+    end
+    
+    test "another another simple example" do 
+      bushes = [10, {6, 3}, 12, {4, 3}]
+      assert BushAlg.build_merge(bushes) == { {10, {6, 3}}, {12, {4, 3}}}
+    end
+  end
+
+  test "ubertester" do 
+    all = for z <- 0..1000 do 
+      rands = for y <- 0..Enum.random(10..5), do: Enum.random(0..20)
+      {length(rands), BushAlg.height(BushAlg.build(rands)), BushAlg.height(BushAlg.build_merge(rands))} 
+    end |> List.keysort(0)
+
+    build_better = Enum.filter(all, fn({len, b, bm}) -> b < bm end) 
+    length(build_better)
+
+    build_merge_better = Enum.filter(all, fn({len, b, bm}) -> b > bm end) 
+    length(build_merge_better)
+
+    equal = Enum.filter(all, fn({len, b, bm}) -> b == bm end) 
+    length(equal)
+
   end
 end
